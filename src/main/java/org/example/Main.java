7111a48eedcf3f4c622ea0116e118c62b9539547
Artem.don <dontsovki@gmail.com>
2023-11-30 18:55:36 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.Package.JButton_Class;
+import org.example.Package.JLabel_Class;
+import org.example.Package.JScrollBar_Class;
+import org.example.Package.JTextField_Class;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        JTextField_Class.JTextField();
+//        JButton_Class.JButton();
+        JLabel_Class.JLabel();
+
+    }
+
+}
---
+++ src/main/java/org/example/Package/JComboBox_Class.java
@@ -0,0 +1,21 @@
+package org.example.Package;
+
+import javax.swing.*;
+
+public class JComboBox_Class extends JPanel_Class{
+    JFrame a;
+    JComboBox_Class(){
+        a = new JFrame("example");
+        String[] courses = { "core java","advance java", "java servlet"};
+        JComboBox c = new JComboBox(courses);
+        c.setBounds(90,90,90,20);
+        a.add(c);
+        a.setSize(400,400);
+        a.setLayout(null);
+        a.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JComboBox_Class();
+    }
+}
